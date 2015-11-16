65d5f2633891b66913ed5c68978d58ebd06eb138
FredericoSilva015 <fredsilva015@gmail.com>
2015-11-16 18:06:07 +0000

    Object selection and rudimentary pathing
    In this code the selection of objects changes their color and alows
    movement, the selection can be in a sinlge click, or a drag box for
    multiple items

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+    //obj canvas
+    public Canvas quitMenu;
+    public Canvas startMenu;
+    public Canvas optionMenu;
+
+    //obj button
+    public Button starButton;
+    public Button optionButton;
+    public Button exitButton;
+
+	void Start () 
+    {
+        //selecção dos canvas no unity
+        startMenu = startMenu.GetComponent<Canvas>();
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        optionMenu = optionMenu.GetComponent<Canvas>();
+
+        //selecção dos button no unity
+        starButton = starButton.GetComponent<Button>();
+        optionButton = optionButton.GetComponent<Button>();
+        exitButton = exitButton.GetComponent<Button>();
+
+        //Menu option/quit iniciam desligados
+        quitMenu.enabled = false;
+        optionMenu.enabled = false;
+	}
+
+    public void BacktoStart()
+    {
+        //desliga o menu de quit/options e liga o menu de start
+        startMenu.enabled = true;
+        quitMenu.enabled = false;
+        optionMenu.enabled = false;
+
+        //aciona os botoes do menu start
+        starButton.enabled = true;
+        optionButton.enabled = true;
+        exitButton.enabled = true;
+
+    }
+
+    public void ExitPress()
+    {
+        //desliga o menu de start e liga o menu de quit
+        startMenu.enabled = false;
+        quitMenu.enabled = true;
+        optionMenu.enabled = false;
+
+        //desliga os butoes do menu start
+        starButton.enabled = false;
+        optionButton.enabled = false;
+        exitButton.enabled = false;
+   
+    }
+    public void OptionPress()
+    {
+        //desliga o menu de start/quit e liga o menu de option
+        startMenu.enabled = false;
+        quitMenu.enabled = false;
+        optionMenu.enabled = true;
+
+        //desliga os butoes do menu start
+        starButton.enabled = false;
+        optionButton.enabled = false;
+        exitButton.enabled = false;
+    }
+
+    public void StartLevel()
+    { 
+        //inicio do jogo
+    }
+
+    public void Quit()
+    {
+        //o jogo fecha
+        Application.Quit();
+    }
+	
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour {
+
+    //vars para selecção
+    public bool selected=false;
+
+    //vars para movimentos
+    private Vector3 moveToDest = Vector3.zero;
+    public float floorOffSet = 1;
+
+    private bool selectedByClick = false;
+
+
+	void Update () 
+    {        
+        //se os obejectos estiverem dentro da box de slecção, determinada pela camera e se o botão estiver pressionado
+        if (GetComponent<Renderer>() && Input.GetMouseButton(0))
+        {
+            if (!selectedByClick)
+            {
+                //retorna basicamente o que a camera ve como um colecção de pontos
+                Vector3 camPos = Camera.main.WorldToScreenPoint(transform.position);
+                //inverte a coordenada dos Y indo ao script CameraOperator          
+                camPos.y = CameraOperator.InvertMouseY(camPos.y);        
+                //se estiver contido na caixa de selecção, passa a true
+                selected = CameraOperator.selection.Contains(camPos);
+            }
+
+            //caso seja seleccionado, a cor do objecto é modificada, caso contrário mantem o valor
+            if (selected)
+                GetComponent<Renderer>().material.color = Color.red;
+            else
+                GetComponent<Renderer>().material.color = Color.white;
+        }   
+        
+        if (selected && Input.GetMouseButtonUp(1))
+        {
+            //Obtem o destino do click direito
+            Vector3 destination = CameraOperator.GetDestination();
+
+            //se o destino for um valor diferente de zero
+            if (destination != Vector3.zero)
+            {
+                //inicia o pathing utilizand Navigation do Unity
+                gameObject.GetComponent<NavMeshAgent>().SetDestination(destination);
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        selectedByClick = true;
+        selected = true;
+    }
+
+    private void OnMouseUp()
+    {
+        if (selectedByClick)
+            selected = true;
+            selectedByClick = false;
+
+    }
+}
